d08976cbab48d701c3e5dbfe0719b2d70920204b
Piotrek92PL <piotrb1992@gmail.com>
2023-11-19 20:25:23 +0100

    Refactor controllers into separate files for improved modularity

--- routes/api/contacts.routes.js
+++ routes/api/contacts.routes.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const contactsController = require('../../controllers/contacts.controller');
 
-router.get('/', contactsController.listContacts);
-router.get('/:contactId', contactsController.getContactById);
-router.post('/', contactsController.addContact);
-router.delete('/:contactId', contactsController.removeContact);
-router.put('/:contactId', contactsController.updateContact);
-router.patch('/:contactId/favorite', contactsController.updateFavoriteStatus);
+const listContacts = require('../../controllers/listContacts.controller');
+const getContactById = require('../../controllers/getContactById.controller');
+const addContact = require('../../controllers/addContact.controller');
+const removeContact = require('../../controllers/removeContact.controller');
+const updateContact = require('../../controllers/updateContact.controller');
+const updateFavoriteStatus = require('../../controllers/updateFavoriteStatus.controller');
 
+router.get('/', listContacts);
+router.get('/:contactId', getContactById);
+router.post('/', addContact);
+router.delete('/:contactId', removeContact);
+router.put('/:contactId', updateContact);
+router.patch('/:contactId/favorite', updateFavoriteStatus);
 
 module.exports = router;
